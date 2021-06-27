8c6c9af8cbd75ccaac803dbbab0a1a797687d4c6
Humberto Marcone <hmrcone@gmail.com>
2021-06-27 14:17:31 -0300

    Criando a controller de user.

---
+++ ConfigArchBackEndNetCore/Models/Users/LoginViewModelInput.cs
@@ -0,0 +1,8 @@
+﻿namespace ConfigArchBackEndNetCore.Models.Users
+{
+    public class LoginViewModelInput
+    {
+        public string Login { get; set; }
+        public string PassWord { get; set; }
+    }
+}
---
+++ ConfigArchBackEndNetCore/Models/Users/RegisterViewModelInput.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ConfigArchBackEndNetCore.Models.Users
+{
+    public class RegisterViewModelInput
+    {
+        [Required(ErrorMessage = "O login é obrigatório!")]
+        public string Login { get; set; }
+        [Required(ErrorMessage = "A senha é obrigatório!")]
+        public string PassWord { get; set; }
+        [Required(ErrorMessage = "O e-mail é obrigatório!")]
+        public string Email { get; set; }
+    }
+}
